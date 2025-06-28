94b395f7b08107daed0d897c8bb344b9e979ca2e
zfy <3116449357@qq.com>
2025-06-28 21:38:33 +0800

    rom.v修正

--- rtl/core/rom.v
+++ rtl/core/rom.v
@@ -4,7 +4,7 @@ module rom(
           //to iftech
           output reg [31:0] inst_o
 );
-          reg [31:0] rom_mem[0:11];     //16KB
+          reg [31:0] rom_mem[0:4095];     //16KB = 16384字节 = 4096个32位字
           always @(*)begin
                     inst_o = rom_mem[rom_addr_i >> 2];
           end
