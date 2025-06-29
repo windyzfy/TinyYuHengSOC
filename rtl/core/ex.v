daf22cae920bda4d1ec135624d3c750296812c73
zfy <3116449357@qq.com>
2025-06-29 20:41:01 +0800

    实现R型add sub指令

--- rtl/core/ex.v
+++ rtl/core/ex.v
@@ -18,12 +18,18 @@ module ex(
     wire [2:0] func3;
     wire [4:0] rs1;
     wire [11:0] imm;
-    //addi指令
+
+    wire [6:0] func7;
+    wire [4:0] rs2;
+    //I型指令
     assign opcode = inst_i[6:0];
     assign rd     = inst_i[11:7];
     assign func3  = inst_i[14:12];
     assign rs1    = inst_i[19:15];
     assign imm    = inst_i[31:20];
+    //R型指令
+    assign func7  = inst_i[31:25];
+    assign rs2    = inst_i[24:20];
     always @(*)begin
         case(opcode)
             `INST_TYPE_I : begin
@@ -40,6 +46,27 @@ module ex(
                     end
                 endcase
             end
+            `INST_TYPE_R_M : begin
+                case(func3)
+                    `INST_ADD_SUB : begin
+                        if(func7 == 7'b000_0000)begin   //add指令
+                            rd_data_o = op1_i + op2_i;
+                            rd_addr_o = rd_addr_i;
+                            reg_wen_o = 1'b1;
+                        end
+                        else begin      //sub指令
+                            rd_data_o = op2_i - op1_i;
+                            rd_addr_o = rd_addr_i;
+                            reg_wen_o = 1'b1;
+                        end
+                    end
+                    default : begin
+                        rd_data_o = 32'b0;
+                        rd_addr_o = 5'b0;
+                        reg_wen_o = 1'b0;
+                    end
+                endcase
+            end            
             default : begin
                 rd_data_o = 32'b0;
                 rd_addr_o = 5'b0;
--- rtl/core/id.v
+++ rtl/core/id.v
@@ -22,17 +22,24 @@ module id(
     wire [2:0] func3;
     wire [4:0] rs1;
     wire [11:0] imm;
-    //addi指令
+
+    wire [6:0] func7;
+    wire [4:0] rs2;
+    //I型指令
     assign opcode = inst_i[6:0];
     assign rd     = inst_i[11:7];
     assign func3  = inst_i[14:12];
     assign rs1    = inst_i[19:15];
     assign imm    = inst_i[31:20];
+    //R型指令
+    assign func7  = inst_i[31:25];
+    assign rs2    = inst_i[24:20];
 
     always @(*)begin
         inst_o = inst_i;
         inst_addr_o = inst_addr_i;
         case(opcode)
+            //I型指令
             `INST_TYPE_I : begin          
                 case(func3) 
                     `INST_ADDI : begin
@@ -54,6 +61,29 @@ module id(
                         rd_addr_o  = 5'b0;                                                            
                     end
                 endcase
+            end
+            //R型指令 & M型指令
+            `INST_TYPE_R_M : begin
+                case(func3) 
+                    `INST_ADD_SUB : begin   //add 还是 sub需要通过func7进一步判断
+                        rs1_addr_o = rs1;
+                        rs2_addr_o = rs2;
+                        //传操作数
+                        op1_o      = rs1_data_i;
+                        op2_o      = rs2_data_i;
+                        //写回操作
+                        reg_wen    = 1'b1;
+                        rd_addr_o  = rd;
+                    end
+                    default : begin
+                        rs1_addr_o = 5'b0;
+                        rs2_addr_o = 5'b0;
+                        op1_o      = 32'b0;
+                        op2_o      = 32'b0;
+                        reg_wen    = 1'b0;
+                        rd_addr_o  = 5'b0;                                                            
+                    end
+                endcase                
             end
             default: begin
                 rs1_addr_o = 5'b0;
